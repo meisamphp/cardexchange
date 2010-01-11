2bdef8aad3b1da7216363de0a596d914ad19bffe
witold.sowa <witold.sowa@a9c5c6b6-c270-11de-a250-c7f789e21d83>
2010-01-11 03:13:32 +0000

    protocol: add Cell LAC field

--- CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/ClientData.java
+++ CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/ClientData.java
@@ -2,13 +2,12 @@ package agh.mobile.contactexchange.protocol;
 
 import java.io.Serializable;
 import java.util.EnumMap;
-import java.util.Map;
 
 public class ClientData implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	public long time;
-	public Map<ClientLocationType, ClientLocation> locations =
+	public EnumMap<ClientLocationType, ClientLocation> locations =
 		new EnumMap<ClientLocationType, ClientLocation>(ClientLocationType.class);
 	public Payload payload = new Payload();
 }
