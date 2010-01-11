fd171c21d28c2bf5d67c0ce1501a675acfb7d4d5
witold.sowa <witold.sowa@a9c5c6b6-c270-11de-a250-c7f789e21d83>
2010-01-11 02:33:41 +0000

    client lib: add a few serialVersionUID fields to shut up eclipse

--- CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/ClientLocation.java
+++ CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/ClientLocation.java
@@ -3,6 +3,8 @@ package agh.mobile.contactexchange.protocol;
 import java.io.Serializable;
 
 public class ClientLocation implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	public double latitude; // in degrees
 	public double longitude; // in degrees
 	public double accuracy; // in meters
