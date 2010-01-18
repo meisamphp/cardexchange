11c1fe9f9e07abbf88f8fd272b3c84404925fb3b
witold.sowa <witold.sowa@a9c5c6b6-c270-11de-a250-c7f789e21d83>
2010-01-18 15:14:14 +0000

    protocol: send raw binary data over the network instead of serialized java objects so protocol implementation in other languages than Java should be much easier..

--- trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/ClientData.java
+++ trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/ClientData.java
@@ -1,12 +1,15 @@
 package agh.mobile.contactexchange.protocol;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
-import java.util.EnumMap;
 
 public class ClientData implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	/**
 	 * NTP time of pushing "Exchange"
 	 */
@@ -43,4 +46,42 @@ public class ClientData implements Serializable {
 	 * Client contact info like name or phone number
 	 */
 	public Payload payload = new Payload();
+
+	public void fromByteArray(byte[] data) throws IOException {
+		ByteArrayInputStream bais = new ByteArrayInputStream(data);
+		DataInputStream dis = new DataInputStream(bais);
+		
+		time = dis.readLong();
+		gpsLatitude = dis.readDouble();
+		gpsLongitude = dis.readDouble();
+		gpsAccuracy = dis.readDouble();
+		cellularLatitude = dis.readDouble();
+		cellularLongitude = dis.readDouble();
+		cellularAccuracy = dis.readDouble();
+		cellId = dis.readLong();
+		cellLac = dis.readLong();
+		
+		byte [] pl = new byte[dis.available()];
+		dis.read(pl);
+		payload.fromByteArray(pl);
+	}
+
+	
+	public byte[] toByteArray() throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DataOutputStream dos = new DataOutputStream(baos);
+
+		dos.writeLong(time);
+		dos.writeDouble(gpsLatitude);
+		dos.writeDouble(gpsLongitude);
+		dos.writeDouble(gpsAccuracy);
+		dos.writeDouble(cellularLatitude);
+		dos.writeDouble(cellularLongitude);
+		dos.writeDouble(cellularAccuracy);
+		dos.writeLong(cellId);
+		dos.writeLong(cellLac);
+		dos.write(payload.toByteArray());
+
+		return baos.toByteArray();
+	}
 }
--- trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/Payload.java
+++ trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/Payload.java
@@ -1,6 +1,11 @@
 package agh.mobile.contactexchange.protocol;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.Serializable;
+import java.io.IOException;
 
 public class Payload implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -9,4 +14,24 @@ public class Payload implements Serializable {
 	public String phoneNumber;
 	
 	//TODO: extend
+
+	
+
+	public void fromByteArray(byte[] data) throws IOException {
+		ByteArrayInputStream bais = new ByteArrayInputStream(data);
+		DataInputStream dis = new DataInputStream(bais);
+	
+		name = dis.readUTF();
+		phoneNumber = dis.readUTF();
+	}
+	
+	public byte[] toByteArray() throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DataOutputStream dos = new DataOutputStream(baos);
+		
+		dos.writeUTF(name);
+		dos.writeUTF(phoneNumber);
+
+		return baos.toByteArray();
+	}
 }
