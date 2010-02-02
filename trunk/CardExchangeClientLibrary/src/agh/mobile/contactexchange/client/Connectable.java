f11448115f37cede4d7bc35c74d4bb5cc96f3353
witold.sowa <witold.sowa@a9c5c6b6-c270-11de-a250-c7f789e21d83>
2010-02-02 23:33:20 +0000

    client lib: code comments

--- trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/client/Connectable.java
+++ trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/client/Connectable.java
@@ -4,20 +4,44 @@ import java.util.Map;
 
 import agh.mobile.contactexchange.protocol.Payload;
 
+/**
+ * This interface must be implemented by Card Exchange clients in
+ * order to be notified about messages received from the server.
+ * Each of these methods may be called right after new data was received.
+ * 
+ * @author wsowa
+ *
+ */
 interface Connectable {
 
-	// called when list of possible exchange partners arrived from the server
+	/**
+	 * This method is called when list of possible exchange partners
+	 * arrived from the server.
+	 * 
+	 * @param partners list that arrived from the server.
+	 */
 	void handlePartnersList(Map<Integer, String> partners);
 	
-	// called when payload data of chosen partner arrived from server
+	/**
+	 * This method is called when exchange data of another client
+	 * arrived from the server.
+	 * 
+	 * @param payload another client exchange data
+	 */
 	void handlePayload(Payload payload);
 	
-	// called when client connection to server has timed out
+	/**
+	 * This method is called when TIMEOUT message is received.
+	 */
 	void handleTimedout();
 	
-	// called when selected partner denied exchange
+	/**
+	 * This method is called when EXCHANGE_DENIED message is received.
+	 */
 	void handleExchangeDenied();
 	
-	// called when client disconnects from server
+	/**
+	 * This method is called when has disconnected from the server.
+	 */
 	void handleDisconnected();
 }
--- trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/client/example/ConsoleExampleClient.java
+++ trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/client/example/ConsoleExampleClient.java
@@ -30,34 +30,56 @@ public class ConsoleExampleClient extends AbstractClient {
 		cd.payload.phoneNumber = phone;
 	}
 	
-
+	
+	/**
+	 * Do all client job
+	 * 
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
 	private void go() throws UnknownHostException, IOException {
 		while (!finished) {
+			// read data from server
 			getConnection().readData();
+			// send data if hasn't already
 			if(!dataSent) {
+				// set NTP time of exchange
 				cd.time = getNtpTime();
-				cd.cellularLatitude = 50.061389; // krakow
+
+				// set own location (krakow)
+				cd.cellularLatitude = 50.061389;
 				cd.cellularLongitude = 19.938333;
+				
+				//send own data
 				getConnection().sendClientData(cd);
 				dataSent = true;
 			}
 			else if (partners == null) {
-				//wait for partners list;
+				// wait for partners list;
+				// when the partners list will arrive, the partners variable 
+				// will be set in handlePartnersList() method.
 			}
 			else if (partners.size() == 0) {
+				// if received zero partners, finish client program
 				System.out.println("No matching partners");
 				finished = true;
 			}
 			else if (!pairIdSent) {
+				// if received one or more partners, print the list and ask
+				// for partner id to exchange
 				System.out.println("Select pertner id:");
 			    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 			    int id = Integer.decode(in.readLine());
+			    
+			    // sent selected partner id
 			    getConnection().sendPairId(id);
 			    pairIdSent = true;
 			    System.out.println("Pair id sent");
 			}
 			else if (payload == null) {
-				//wait for payload
+				// wait for payload.
+				// when the payload of selected partner will arrive, the payload
+				// variable will be set in handlePayload() method.
 			}
 		}
 		
@@ -74,6 +96,7 @@ public class ConsoleExampleClient extends AbstractClient {
 	}
 
 	public void handlePartnersList(Map<Integer, String> partners) {
+		// store received partners list
 		this.partners = partners;
 		System.out.println("Reveived partners list:");
 		for(Entry<Integer, String> e : partners.entrySet()) {
@@ -82,6 +105,7 @@ public class ConsoleExampleClient extends AbstractClient {
 	}
 
 	public void handlePayload(Payload payload) {
+		// store received partner's payload
 		this.payload = payload;
 		System.out.println("Received payload:");
 		System.out.println("Name:  " + payload.name);
@@ -94,20 +118,20 @@ public class ConsoleExampleClient extends AbstractClient {
 		finished = true;
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		if (args.length != 2 && args.length != 4) {
 			printUsage();
 			return;
 		}
+		
+		// create client
 		try {
 			ConsoleExampleClient client = new ConsoleExampleClient(args[0], args[1]);
 			if (args.length == 4)
 				client.getConnection().connect(args[2], Integer.decode(args[3]));
 			else
 				client.getConnection().connect(client.getServerAddress());
+			// start client.
 			client.go();
 		}
 		catch (Exception e) {
