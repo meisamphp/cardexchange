ade48a5e908dd06868e9da1ebf4d0274922bdfb3
witold.sowa <witold.sowa@a9c5c6b6-c270-11de-a250-c7f789e21d83>
2010-02-02 22:23:33 +0000

    protocol: code comments

--- trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/MessageType.java
+++ trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/MessageType.java
@@ -1,10 +1,75 @@
 package agh.mobile.contactexchange.protocol;
 
+/**
+ * Types of messages being send between client and server.
+ * All messages are in format:
+ * int32: type
+ * int32: body length
+ * variant: message body
+ * 
+ * All numeric data are signed and bigendian. Strings are UTF8 byte arrays
+ * preceded with 2-byte unsigned bigendian integer conteining number of bytes
+ * in the array.
+ * 
+ * Correct messages flow is:
+ * <ol>
+ * <li> CLIENT_DATA
+ * <li> PARTNERS_LIST
+ * <li> PAIR_ID
+ * <li> PAYLOAD
+ * </ol>
+ * 
+ * Alternative messages flow is:
+ * <ol>
+ * <li> CLIENT_DATA
+ * <li> PARTNERS_LIST
+ * <li> EXCHANGE_DENIED
+ * </ol>
+ * 
+ * Messages exchange can be interrupted at any moment by the TIMEDOUT
+ * message sent from the server to a client.
+ * 
+ * @author wsowa
+ */
 public interface MessageType {
+	
+	/**
+	 * Contains client NTP time, location data and data to exchange.
+	 * This message is sent from a client to the server. The length of
+	 * the message body is variant, but is at least 56 bytes.
+	 */
 	public static final int CLIENT_DATA     = 0;
+	
+	/**
+	 * Contains list of pairs (id, name) with partners that client
+	 * can pair with. This message is sent from the server to a client.
+	 * The length of the message body is variant, but is at least 4 bytes.
+	 */
 	public static final int PARTNERS_LIST   = 1; 
+	
+	/**
+	 * Contains one integer with id of partners client that the client
+	 * is willing to exchange with. This message is sent from a client
+	 * to the server. The length of the message body is equal 4 bytes.
+	 */
 	public static final int PAIR_ID         = 2;
+	
+	/**
+	 * Contains data to exchange coming from another client. This message
+	 * is sent from the server to a client. The length of the message body
+	 * is variant.
+	 */
 	public static final int PAYLOAD         = 3;
+	
+	/**
+	 * Indicates that client was inactive too long. This message is sent
+	 * from the server to a client and has no body.
+	 */
 	public static final int TIMEDOUT        = 4;
+	
+	/**
+	 * Indicates that selected client didn't accept exchange. This message
+	 * is sent from the server to a client and has no body.
+	 */
 	public static final int EXCHANGE_DENIED = 5;
 }
--- trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/PairsList.java
+++ trunk/CardExchangeClientLibrary/src/agh/mobile/contactexchange/protocol/PairsList.java
@@ -8,30 +8,59 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+/**
+ * Pairs list  message. This message contains a list of clients that
+ * a client can be paired with.
+ * 
+ * @author wsowa
+ */
 public class PairsList extends HashMap<Integer, String> {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Deserialize a message
+	 * 
+	 * @param data Byte array of message body to deserialize from
+	 * @throws IOException error occurred while reading bytes 
+	 */
 	public void fromByteArray(byte[] data) throws IOException {
 		ByteArrayInputStream bais = new ByteArrayInputStream(data);
 		DataInputStream dis = new DataInputStream(bais);
 	
+		// read number of partners in message
 		int n = dis.readInt();
+		
 		for (int i = 0; i < n; i++) {
+			// read partner client id
 			int id = dis.readInt();
+			
+			// read partner name
 			String name = dis.readUTF();
+			
+			// put id and name to dictionary
 			put(id, name);
 		}
 	}
 	
+	/**
+	 * Serialize a message
+	 * 
+	 * @return Byte array with serialized message body
+	 * @throws IOException error occurred while writing bytes 
+	 */
 	public byte[] toByteArray() throws IOException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		DataOutputStream dos = new DataOutputStream(baos);
 		
+		// write the int32 with number of partners first.
 		int n = size();
 		dos.writeInt(n);
 		
+		// for each partner
 		for (Entry<Integer, String> e : entrySet()) {
+			// write int32 with partner client id 
 			dos.writeInt(e.getKey());
+			// string with partner name
 			dos.writeUTF(e.getValue());
 		}
 
