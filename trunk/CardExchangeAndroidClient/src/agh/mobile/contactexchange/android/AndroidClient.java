56b88267cbd54101a670324e96bf8223d38baf19
witold.sowa <witold.sowa@a9c5c6b6-c270-11de-a250-c7f789e21d83>
2010-02-03 00:49:51 +0000

    android: code comments

--- trunk/CardExchangeAndroidClient/src/agh/mobile/contactexchange/android/AndroidClient.java
+++ trunk/CardExchangeAndroidClient/src/agh/mobile/contactexchange/android/AndroidClient.java
@@ -15,24 +15,91 @@ import android.os.Message;
 import android.util.Log;
 
 
-
+/**
+ * Android exchange client implementation. It runs as a separate
+ * thread waiting for data from server.
+ * 
+ * @author wsowa
+ *
+ */
 public class AndroidClient extends AbstractClient implements Runnable {
 	
+	/**
+	 * Client states
+	 * 
+	 * @author wsowa
+	 *
+	 */
 	enum State {
+		
+		/**
+		 * Client is stopped
+		 */
 		STOPPED,
+		
+		/**
+		 * Client is conneted to server 
+		 */
 		CONNECTED,
+		
+		/**
+		 * Client sent own data 
+		 */
 		DATA_SENT,
+		
+		/**
+		 * Client received partners list 
+		 */
 		GOT_PARTNERS,
-		PAIR_SENT,		
-		GOT_CARD,		
+		
+		
+		/**
+		 * Client sent partner selection
+		 */
+		PAIR_SENT,
+		
+		/**
+		 * Client receiver partners data
+		 */
+		GOT_CARD,
+		
+		/**
+		 * Client finished to work 
+		 */
 		FINISHED
 	}
 	
+	/**
+	 * Reason of client state change
+	 * 
+	 * @author wsowa
+	 *
+	 */
 	enum Reason {
+		
+		/**
+		 * No reason of change. Change was typical client behavior. 
+		 */
 		NO_REASON,
+		
+		/**
+		 * Client couldn't resolve host name
+		 */
 		UNKNOWN_HOST,
+		
+		/**
+		 * Client encountered an error in data transmission.
+		 */
 		IO_ERROR,
+		
+		/**
+		 * Client has timed out (received TIMEOUT message form the server). 
+		 */
 		TIMEDOUT,
+		
+		/**
+		 * Client was denied to exchange (received "denied" message).
+		 */
 		DENIED
 	}
 	
@@ -68,10 +135,13 @@ public class AndroidClient extends AbstractClient implements Runnable {
 		
 		HashMap<Integer, String> pairs = new HashMap<Integer, String>(partners);
 		
+		// change state to GOT_PARTNERS
 		msg.arg1 = State.GOT_PARTNERS.ordinal();
 		msg.arg2 = Reason.NO_REASON.ordinal();
 		
+		// attach partners list to state change message
 		data.putSerializable("partners", pairs);
+		
 		msg.setData(data);
 		handler.sendMessage(msg);
 	}
@@ -80,12 +150,15 @@ public class AndroidClient extends AbstractClient implements Runnable {
 		Message msg = new Message();
 		Bundle data = new Bundle();
 		
+		// change state to GOT_CARD
 		msg.arg1 = State.GOT_CARD.ordinal();
 		msg.arg2 = Reason.NO_REASON.ordinal();
 		
 		UserSettings card = new UserSettings(payload);
 		
+		// attach partner's card to state change message
 		data.putSerializable("card", card);
+		
 		msg.setData(data);
 		handler.sendMessage(msg);
 
@@ -95,10 +168,21 @@ public class AndroidClient extends AbstractClient implements Runnable {
 		setState(State.STOPPED, Reason.TIMEDOUT);
 	}
 
+	/**
+	 * Set client state. Has the same effect like 
+	 * {@link #setState(State, Reason)} with NO_REASON second argument
+	 * 
+	 * @param state a new state.
+	 */
 	private void setState(State state) {
 		setState(state, Reason.NO_REASON);
 	}
 	
+	/**
+	 * Set client state.
+	 * @param state a new state.
+	 * @param reason a reason of state change.
+	 */
 	private void setState(State state, Reason reason) {
 		Message m = new Message();
 		m.arg1 = state.ordinal();
@@ -106,9 +190,17 @@ public class AndroidClient extends AbstractClient implements Runnable {
 		handler.sendMessage(m);
 	}
 	
+	/**
+	 * Connect to the exchange server.
+	 * 
+	 * @throws Exception if error occurred
+	 */
 	private void connect() throws Exception {
 		try {
+			// initialize client first (this gets NTP time offset for example)
 			init();
+			
+			// connect to server.
 			getConnection().connect(getServerAddress());
 			isConnected = true;
 			setState(State.CONNECTED);
@@ -123,9 +215,14 @@ public class AndroidClient extends AbstractClient implements Runnable {
 		}
 	}
 	
+	/**
+	 * Send own data to the server.
+	 */
 	public void sendData() {
+		// put NTP time of exchange to the message
 		cd.time = getNtpTime(exchangeTime);
 		try {
+			//sent message
 			getConnection().sendClientData(cd);
 			setState(State.DATA_SENT);
 		} catch (IOException e) {
@@ -135,6 +232,10 @@ public class AndroidClient extends AbstractClient implements Runnable {
 		}
 	}
 	
+	/**
+	 * Sent selected partner id
+	 * @param id id of selected partner
+	 */
 	public void selectPair(int id) {
 		 try {
 			getConnection().sendPairId(id);
@@ -147,27 +248,41 @@ public class AndroidClient extends AbstractClient implements Runnable {
 	}
 	
 	public void run() {
+		//set initial client state
 		setState(State.STOPPED);
+		
 		try {
+			// connect to server
 			connect();
 		}
 		catch (Exception e) {
 			return;
 		}
 		
+		// main client loop.
 		while (isConnected)
 			try {
+				// try to read data from server.
 				getConnection().readData();
 			} catch (IOException e) {
 				setState(State.STOPPED, Reason.IO_ERROR);
 				disconnect();
 				e.printStackTrace();
 			}
+			
+		// when the loop is finished. put client to finished state.
 		setState(State.FINISHED);
 	}
 
+	/**
+	 * Set client location data
+	 * 
+	 * @param gpsLocation client location according to GPS.
+	 * @param cellularLocation client location according to Cellular estimation.
+	 */
 	public void setLocation(Location gpsLocation, Location cellularLocation) {
 		
+		// set GPS location data
 		if(gpsLocation != null) {
 			cd.gpsLatitude = gpsLocation.getLatitude();
 			cd.gpsLongitude = gpsLocation.getLongitude();
@@ -175,8 +290,10 @@ public class AndroidClient extends AbstractClient implements Runnable {
 			Log.i("gpsLoc", "" + cd.gpsLatitude + " " + cd.gpsLongitude + " " + cd.gpsAccuracy);
 		}
 		else
+			// or indicate that GPS position is not available
 			cd.gpsAccuracy = -1.0;
 		
+		// set cellular location data
 		if(cellularLocation != null) {
 			cd.cellularLatitude = cellularLocation.getLatitude();
 			cd.cellularLongitude = cellularLocation.getLongitude();
@@ -184,18 +301,33 @@ public class AndroidClient extends AbstractClient implements Runnable {
 			Log.i("cellLoc", "" + cd.cellularLatitude + " " + cd.cellularLongitude + " " + cd.cellularAccuracy);
 		}
 		else
+			// or indicate that cellular position is not available
 			cd.cellularAccuracy = -1.0;
 	}
 
+	/**
+	 * Set own vCard.
+	 * 
+	 * @param settings own contact information to exchange
+	 */
 	public void setSettings(UserSettings settings) {
 		cd.payload.name = settings.getName();
 		cd.payload.phoneNumber = settings.getPhone();		
 	}
 	
+	/**
+	 * Set time of pushing "exchange" button. Given time will be adjusted by
+	 * NTP time offset
+	 * 
+	 * @param time local time of pushing the button.
+	 */
 	public void setExchangeTime(long time) {
 		exchangeTime = time;	
 	}
 
+	/**
+	 * Disconnect client from the server.
+	 */
 	public void disconnect() {
 		if (!isConnected)
 			return;
@@ -206,8 +338,6 @@ public class AndroidClient extends AbstractClient implements Runnable {
 			e.printStackTrace();
 		}
 		isConnected = false;
-
 	}
-
 
 }
